7f49acfc029d1cff92407571a6eaf9e19423dbbc
WhippyCleric <WhippyCleric@gmail.com>
2016-05-21 17:27:18 +0200

    Implement Free Kicks

--- TASPremierGameSim/src/main/java/com/whippy/tas/premier/game/sim/GameSimulator.java
+++ TASPremierGameSim/src/main/java/com/whippy/tas/premier/game/sim/GameSimulator.java
@@ -28,27 +28,27 @@ public class GameSimulator {
 	private static int CHANCE_OF_ACTION = 30;
 	private static Random rand = new Random();
 	private static Map<Integer, MatchAction> actionMap = new HashMap<Integer,MatchAction>();
-	
+
 	public static void main(String[] args) throws IOException{
 		List<String> linesOfPlayers = Utils.getNames("TAS Premier League Player List - players.csv", GameSimulator.class);
 		List<Player> players = new ArrayList<Player>();
-		
+
 		for (String playerLine : linesOfPlayers) {
 			List<String> attributes = Arrays.asList(playerLine.split(","));
 			if(!attributes.get(0).equals("Name")){
 				players.add(PlayerBuilder.buildPlayer(attributes));
 			}
 		}
-		
+
 		TeamBuilder teamBuilder = new TeamBuilder(players);
 		Team team1 = teamBuilder.buildTeam("team1");
 		Team team2 = teamBuilder.buildTeam("team2");
 		buildActionMap();
-		
-		
+
+
 		System.out.println(simulateGame(team1, team2).toString());
 	}
-	
+
 	public static MatchReport simulateGame(Team home, Team away){
 		MatchReport matchReport = new MatchReport(home, away);
 		matchReport.pushMatchEvent(new MatchEvent("here, lets get underway", 0, MatchAction.FRIST_KICK_OFF));
@@ -65,14 +65,81 @@ public class GameSimulator {
 			}
 		}
 		matchReport.pushMatchEvent(new MatchEvent("" , 90, MatchAction.FULL_TIME));
-		
+
 		return matchReport;
 	}
-	
+
+	public static void figureOutAction(Team home, Team away, MatchReport report, int time){
+		//TODO this should be redone to work with passing and tackline stats
+		MatchAction action = actionMap.get(rand.nextInt(actionMap.size()-1));
+
+		if(action.equals(MatchAction.SHOT)){
+			runRandomShot(home, away, report, time);
+		}else if(action.equals(MatchAction.CORNER)){
+			runCorner(home, away, report, time);
+		}else if(action.equals(MatchAction.FREE_KICK_ATT)){
+			runAttackingFreeKick(home, away, report, time);
+		}else if(action.equals(MatchAction.FREE_KICK_ATT)){	
+			runDefFreeKick(home, away, report, time);
+		}
+	}
+
+	private static void runAttackingFreeKick(Team home, Team away, MatchReport report, int time) {
+		double team1FreeKickChance = getPassingStat(home) + getSpeedStat(home) - getTacklingStat(away);
+		double team2FreeKickChance = getPassingStat(away) + getSpeedStat(away) - getTacklingStat(home);
+		int freeKicker = rand.nextInt((int) Math.floor(team1FreeKickChance + team2FreeKickChance));
+		if(freeKicker<team1FreeKickChance){
+			//Home Free Kick
+			runFoul(home, away, report, time, false);
+			//Odds of a shot are based on setPiece skill
+			Player kickTaker = getSetPieceTaker(home);
+			report.pushMatchEvent(new MatchEvent("to be taken by " + kickTaker.getName() + " for " + home.getTeamName(), time, MatchAction.FREE_KICK_ATT));
+			if(rand.nextInt(kickTaker.getStats().getSetPieces()+100) < kickTaker.getStats().getSetPieces()){				
+				runShotChance(home,away,report,time,true);			
+			}
+		}else{
+			runFoul(home, away, report, time, true);
+			//Away Free Kick
+			//Odds of a shot are based on setPiece skill
+			Player kickTaker = getSetPieceTaker(away);
+			report.pushMatchEvent(new MatchEvent("to be taken by " + kickTaker.getName() + " for " + away.getTeamName(), time, MatchAction.FREE_KICK_ATT));
+			if(rand.nextInt(kickTaker.getStats().getSetPieces()+100) < kickTaker.getStats().getSetPieces()){				
+				runShotChance(home,away,report,time,false);			
+			}
+		}
+	}
+
+	private static void runDefFreeKick(Team home, Team away, MatchReport report, int time) {
+		double team1FreeKickChance = getPassingStat(home) + getSpeedStat(home) - getTacklingStat(away);
+		double team2FreeKickChance = getPassingStat(away) + getSpeedStat(away) - getTacklingStat(home);
+		int freeKicker = rand.nextInt((int) Math.floor(team1FreeKickChance + team2FreeKickChance));
+		if(freeKicker<team1FreeKickChance){
+			//Home Free Kick
+			runFoul(home, away, report, time, false);
+			Player kickTaker = getSetPieceTaker(home);
+			report.pushMatchEvent(new MatchEvent("to be taken by " + kickTaker.getName() + " for " + home.getTeamName(), time, MatchAction.FREE_KICK_DEF));
+		}else{
+			runFoul(home, away, report, time, true);
+			//Away Free Kick
+			Player kickTaker = getSetPieceTaker(away);
+			report.pushMatchEvent(new MatchEvent("to be taken by " + kickTaker.getName() + " for " + away.getTeamName(), time, MatchAction.FREE_KICK_DEF));
+		}
+	}
+
+	private static Player getSetPieceTaker(Team home) {
+		List<Player> players = home.getTheTeam();
+		Player taker = players.get(0);
+		for (Player player : players) {
+			if(player.getStats().getSetPieces()>taker.getStats().getSetPieces()){
+				taker = player;
+			}
+		}
+		return taker;
+	}
+
 	public static void runRandomShot(Team home, Team away, MatchReport report, int time){
-		//In the event of a random goal currently only scoring stat is taken into account
-		double team1ScoreChance = getScoringStat(home);
-		double team2ScoreChance = getScoringStat(away);
+		double team1ScoreChance = getScoringStat(home) + getPassingStat(home) + (getSpeedStat(home)*2) - getTacklingStat(away);
+		double team2ScoreChance = getScoringStat(away) + getPassingStat(away) + (getSpeedStat(away)*2) - getTacklingStat(home);
 		int scorer = rand.nextInt((int) Math.floor(team1ScoreChance + team2ScoreChance));
 		if(scorer<team1ScoreChance){
 			runShotChance(home,away,report,time,true);
@@ -80,39 +147,39 @@ public class GameSimulator {
 			runShotChance(home,away,report,time,false);
 		}
 	}
-	
+
 	public static void runShotChance(Team home, Team away, MatchReport report, int time, boolean isHome){
 		Player goaly;
 		Player shooter;
 		if(isHome){
 			goaly = findGoalKeeper(away);
 			shooter = findShooter(home);
-			report.pushMatchEvent(new MatchEvent(" by " + shooter.getName() + " for " + home.getTeamName(), time, MatchAction.SHOT));
+			report.pushMatchEvent(new MatchEvent("by " + shooter.getName() + " for " + home.getTeamName(), time, MatchAction.SHOT));
 		}else{
 			goaly = findGoalKeeper(home);			
 			shooter = findShooter(away);
-			report.pushMatchEvent(new MatchEvent(" by " + shooter.getName() + " for " + away.getTeamName(), time, MatchAction.SHOT));
+			report.pushMatchEvent(new MatchEvent("by " + shooter.getName() + " for " + away.getTeamName(), time, MatchAction.SHOT));
 		}
 		int scoreChance = rand.nextInt(shooter.getStats().getShooting() + goaly.getStats().getGoalKeeping());
 		if(scoreChance<shooter.getStats().getShooting()){
 			//goal
 			if(isHome){
 				report.homeGoal();
-				report.pushMatchEvent(new MatchEvent(" by " + shooter.getName() + " for " + home.getTeamName(), time, MatchAction.GOAL));
+				report.pushMatchEvent(new MatchEvent("by " + shooter.getName() + " for " + home.getTeamName(), time, MatchAction.GOAL));
 			}else{
 				report.awayGoal();
-				report.pushMatchEvent(new MatchEvent(" by " + shooter.getName() + " for " + away.getTeamName(), time, MatchAction.GOAL));
+				report.pushMatchEvent(new MatchEvent("by " + shooter.getName() + " for " + away.getTeamName(), time, MatchAction.GOAL));
 			}
 		}else{
 			//save
 			if(isHome){
-				report.pushMatchEvent(new MatchEvent(" by " + goaly.getName() + " for " + away.getTeamName(), time, MatchAction.SAVE));
+				report.pushMatchEvent(new MatchEvent("by " + goaly.getName() + " for " + away.getTeamName(), time, MatchAction.SAVE));
 			}else{
-				report.pushMatchEvent(new MatchEvent(" by " + goaly.getName() + " for " + home.getTeamName(), time, MatchAction.SAVE));
+				report.pushMatchEvent(new MatchEvent("by " + goaly.getName() + " for " + home.getTeamName(), time, MatchAction.SAVE));
 			}
 		}
 	}
-	
+
 	public static Player findShooter(Team team){
 		List<Player> possibleShooters = new ArrayList<Player>();
 		List<Player> players = team.getTheTeam();
@@ -130,22 +197,22 @@ public class GameSimulator {
 		}
 		return possibleShooters.get(rand.nextInt(possibleShooters.size()));
 	}
-	
+
 	public static void runCorner(Team home, Team away, MatchReport report, int time){
 		//Figure out whose corner
 		double team1CornerChance = getCornerStat(home);
 		double team2CornerChance = getCornerStat(away);
 		int cornerOwner = rand.nextInt((int) Math.floor(team1CornerChance + team2CornerChance));
 		if(cornerOwner<team1CornerChance){
-			report.pushMatchEvent(new MatchEvent(" for " + home.getTeamName(), time, MatchAction.CORNER));
+			report.pushMatchEvent(new MatchEvent("for " + home.getTeamName(), time, MatchAction.CORNER));
 			runPostCorner(home,away,report,time,true);
 		}else{			
-			report.pushMatchEvent(new MatchEvent(" for " + away.getTeamName(), time, MatchAction.CORNER));
+			report.pushMatchEvent(new MatchEvent("for " + away.getTeamName(), time, MatchAction.CORNER));
 			runPostCorner(home,away,report,time,false);
 		}
 	}
-		
-	
+
+
 	public static void runFoul(Team home, Team away, MatchReport report, int time, boolean isHome){
 		double defenseDirty = 0;
 		if(isHome){
@@ -171,18 +238,18 @@ public class GameSimulator {
 				cardedPlayer = findDirtiestBackPlayer(away);
 			}
 			if(isRed && isHome){
-				report.pushMatchEvent(new MatchEvent(" for " + cardedPlayer.getName() + " " + home.getTeamName(), time, MatchAction.RED));
+				report.pushMatchEvent(new MatchEvent("for " + cardedPlayer.getName() + " " + home.getTeamName(), time, MatchAction.RED));
 			}else if(isRed){
-				report.pushMatchEvent(new MatchEvent(" for " + cardedPlayer.getName() + " " + away.getTeamName(), time, MatchAction.RED));				
+				report.pushMatchEvent(new MatchEvent("for " + cardedPlayer.getName() + " " + away.getTeamName(), time, MatchAction.RED));				
 			}
 			if(isYellow && isHome){
-				report.pushMatchEvent(new MatchEvent(" for " + cardedPlayer.getName() + " " + home.getTeamName(), time, MatchAction.YELLOW));
+				report.pushMatchEvent(new MatchEvent("for " + cardedPlayer.getName() + " " + home.getTeamName(), time, MatchAction.YELLOW));
 			}else if(isYellow){
-				report.pushMatchEvent(new MatchEvent(" for " + cardedPlayer.getName() + " " + away.getTeamName(), time, MatchAction.YELLOW));				
+				report.pushMatchEvent(new MatchEvent("for " + cardedPlayer.getName() + " " + away.getTeamName(), time, MatchAction.YELLOW));				
 			}
 		}
 	}
-	
+
 	public static Player findDirtiestBackPlayer(Team team){
 		List<Player> players = team.getTheTeam();
 		Player dirtyBastad = players.get(0);
@@ -206,13 +273,13 @@ public class GameSimulator {
 		if(isHome){
 			Player player = findBestPenaltyTaker(home);
 			Player goaly = findGoalKeeper(away);
-			report.pushMatchEvent(new MatchEvent(" for " + home.getTeamName() + ". " + player.getName() + " prepares to take the shot, whilst " + goaly.getName() + " awaits his chance to save.", time, MatchAction.PENALTY));
+			report.pushMatchEvent(new MatchEvent("for " + home.getTeamName() + ". " + player.getName() + " prepares to take the shot, whilst " + goaly.getName() + " awaits his chance to save.", time, MatchAction.PENALTY));
 			int scored = rand.nextInt((int) Math.floor(player.getStats().getShooting()  + (goaly.getStats().getGoalKeeping()*1.5)));
 			if(scored<player.getStats().getShooting()){
 				report.pushMatchEvent(new MatchEvent(player.getName() + " scores for " + home.getTeamName(), time, MatchAction.GOAL));
 				report.homeGoal();
 			}else{
-				report.pushMatchEvent(new MatchEvent(" by " + goaly.getName(), time, MatchAction.SAVE));
+				report.pushMatchEvent(new MatchEvent("by " + goaly.getName(), time, MatchAction.SAVE));
 			}
 		}else{
 			Player player = findBestPenaltyTaker(away);			
@@ -222,11 +289,11 @@ public class GameSimulator {
 				report.pushMatchEvent(new MatchEvent(player.getName() + " scores for " + away.getTeamName(), time, MatchAction.GOAL));
 				report.awayGoal();
 			}else{
-				report.pushMatchEvent(new MatchEvent(" by " + goaly.getName(), time, MatchAction.SAVE));
+				report.pushMatchEvent(new MatchEvent("by " + goaly.getName(), time, MatchAction.SAVE));
 			}
 		}
 	}
-	
+
 	private static Player findGoalKeeper(Team away) {
 		Player bestGoaly =  away.getTheTeam().get(0);
 		for (Player player : away.getTheTeam()) {
@@ -251,7 +318,7 @@ public class GameSimulator {
 		}
 		return penaltyTaker;
 	}
-	
+
 	public static void runPostCorner(Team home, Team away, MatchReport report, int time, boolean isHome){
 		int whatHappend = rand.nextInt(100);
 		if(whatHappend < SHOT_FROM_CORNER){
@@ -281,28 +348,18 @@ public class GameSimulator {
 			}
 		}else if(whatHappend < SHOT_FROM_CORNER + FOUL_CHANCE_FROM_CORNER + CORNER_FROM_CORNER){
 			if(isHome){
-				report.pushMatchEvent(new MatchEvent(" for " + home.getTeamName(), time, MatchAction.CORNER));				
+				report.pushMatchEvent(new MatchEvent("for " + home.getTeamName(), time, MatchAction.CORNER));				
 			}else{
-				report.pushMatchEvent(new MatchEvent(" for " + away.getTeamName(), time, MatchAction.CORNER));
 			}
 			runPostCorner(home, away, report, time, isHome);
 		}else{
 			//Nothing comes from the corner
 		}
 	}
-	
-	
-	public static void figureOutAction(Team home, Team away, MatchReport report, int time){
-		MatchAction action = actionMap.get(rand.nextInt(actionMap.size()-1));
-		if(action.equals(MatchAction.SHOT)){
-			runRandomShot(home, away, report, time);
-		}else if(action.equals(MatchAction.CORNER)){
-			runCorner(home, away, report, time);
-		}else{			
-		//	report.pushMatchEvent(new MatchEvent("", time, action));
-		}
-	}
-	
+
+
+
+
 	public static double getDirtyAttackStat(Team team){
 		double dirty = 0;
 		List<Player> players = team.getTheTeam();
@@ -318,7 +375,7 @@ public class GameSimulator {
 		}
 		return dirty;
 	}
-	
+
 	public static double getDirtyDefenseStat(Team team){
 		double dirty = 0;
 		List<Player> players = team.getTheTeam();
@@ -334,7 +391,7 @@ public class GameSimulator {
 		}
 		return dirty;
 	}
-	
+
 	public static double getScoringStat(Team team){
 		double score = 0;
 		List<Player> players = team.getTheTeam();
@@ -348,9 +405,65 @@ public class GameSimulator {
 				score = score + (stats.getShooting() * 0.2);
 			}
 		}
-		return score;
+		return score + getLeadership(team);
 	}
-	
+	public static double getPassingStat(Team team){
+		double pass = 0;
+		List<Player> players = team.getTheTeam();
+		for (Player player : players) {
+			Stats stats = player.getStats();
+			if(stats.getPositions().contains(Position.MIDFIELD)){
+				pass = pass + stats.getPassing();
+			}else if(stats.getPositions().contains(Position.DEFENSE)){
+				pass = pass + (stats.getPassing() * 0.5);
+			}else if(stats.getPositions().contains(Position.FORWARD)){
+				pass = pass + (stats.getPassing() * 0.2);
+			}
+		}
+		return pass + getLeadership(team);
+	}
+	public static double getTacklingStat(Team team){
+		double tackle = 0;
+		List<Player> players = team.getTheTeam();
+		for (Player player : players) {
+			Stats stats = player.getStats();
+			if(stats.getPositions().contains(Position.DEFENSE)){
+				tackle = tackle + stats.getTackling();
+			}else if(stats.getPositions().contains(Position.MIDFIELD)){
+				tackle = tackle + (stats.getTackling() * 0.5);
+			}else if(stats.getPositions().contains(Position.FORWARD)){
+				tackle = tackle + (stats.getTackling() * 0.2);
+			}
+		}
+		return tackle + getLeadership(team);
+	}
+	public static double getSpeedStat(Team team){
+		double speed = 0;
+		List<Player> players = team.getTheTeam();
+		for (Player player : players) {
+			Stats stats = player.getStats();
+			if(stats.getPositions().contains(Position.FORWARD)){
+				speed = speed + stats.getSpeed();
+			}else if(stats.getPositions().contains(Position.MIDFIELD)){
+				speed = speed + (stats.getSpeed() * 0.5);
+			}else if(stats.getPositions().contains(Position.DEFENSE)){
+				speed = speed + (stats.getSpeed() * 0.2);
+			}
+		}
+		return speed + getLeadership(team);
+	}
+
+	public static int getLeadership(Team team){
+		int topLeader = 0;
+		List<Player> players = team.getTheTeam();
+		for (Player player : players) {
+			if(player.getStats().getLeadership()>topLeader){
+				topLeader = player.getStats().getLeadership(); 
+			}
+		}
+		return topLeader;
+	}
+
 
 	public static double getCornerStat(Team team){
 		double corner = 0;
@@ -369,10 +482,10 @@ public class GameSimulator {
 				corner = corner + stats.getTackling();
 			}
 		}
-		return corner;
+		return corner + getLeadership(team);
 	}
-	
-	
+
+
 	private static void buildActionMap(){
 		actionMap.put(0, MatchAction.CORNER);
 		actionMap.put(1, MatchAction.CORNER);
@@ -388,18 +501,13 @@ public class GameSimulator {
 		actionMap.put(11, MatchAction.FREE_KICK_DEF);
 		actionMap.put(12, MatchAction.FREE_KICK_DEF);
 		actionMap.put(13, MatchAction.FREE_KICK_DEF);
-		actionMap.put(14, MatchAction.GOAL);
-		actionMap.put(15, MatchAction.PENALTY);
-		actionMap.put(16, MatchAction.RED);
-		actionMap.put(17, MatchAction.YELLOW);
-		actionMap.put(18, MatchAction.YELLOW);
-		actionMap.put(19, MatchAction.YELLOW);
-		actionMap.put(20, MatchAction.SHOT);
-		actionMap.put(21, MatchAction.SHOT);
-		actionMap.put(22, MatchAction.SHOT);
-		actionMap.put(23, MatchAction.SHOT);
-		actionMap.put(24, MatchAction.SHOT);
+		actionMap.put(14, MatchAction.PENALTY);
+		actionMap.put(15, MatchAction.SHOT);
+		actionMap.put(16, MatchAction.SHOT);
+		actionMap.put(17, MatchAction.SHOT);
+		actionMap.put(18, MatchAction.SHOT);
+		actionMap.put(19, MatchAction.SHOT);
 	}
-	
-	
+
+
 }
